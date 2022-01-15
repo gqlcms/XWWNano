e71d496eeda37e53747a9f35091fbbec1d313ef7
YuzheZhao <60813920+StephenChao@users.noreply.github.com>
2022-01-15 22:41:50 +0800

    Add files via upload

---
+++ TransferTree/config/Input/InputFile_2016.py
@@ -0,0 +1,275 @@
+self.path16 = "/eos/user/y/yusong/qilong/EFT/Merged/VVV_10_6_MERGED/"
+
+self.InputFile = {
+    "QCD_HT1000to1500": "QCD_HT1000to1500_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8.root",
+    "QCD_HT1500to2000": "QCD_HT1500to2000_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8.root",
+    "QCD_HT2000toInf": "QCD_HT2000toInf_TuneCP5_PSWeights_13TeV-madgraphMLM-pythia8.root",
+    "ST_s": "ST_s-channel_4f_leptonDecays_TuneCP5_13TeV-amcatnlo-pythia8.root",
+    "ST_t_channel_antitop": "ST_t-channel_antitop_5f_InclusiveDecays_TuneCP5_13TeV-powheg-pythia8.root",
+    "ST_t_channel_top": "ST_t-channel_top_5f_InclusiveDecays_TuneCP5_13TeV-powheg-pythia8.root",
+    "ST_tW_antitop": "ST_tW_antitop_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8.root",
+    "ST_tW_top": "ST_tW_top_5f_inclusiveDecays_TuneCP5_13TeV-powheg-pythia8.root",
+    "TTTo2L2Nu": "TTTo2L2Nu_TuneCP5_13TeV-powheg-pythia8.root",
+    "TTToSemiLeptonic": "TTToSemiLeptonic_TuneCP5_13TeV-powheg-pythia8.root",
+    "TTWJetsToLNu": "TTWJetsToLNu_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8.root",
+    "TTWJetsToQQ": "TTWJetsToQQ_TuneCP5_13TeV-amcatnloFXFX-madspin-pythia8.root",
+    "TTZToLLNuNu": "TTZToLLNuNu_M-10_TuneCP5_13TeV-amcatnlo-pythia8.root",
+    "TTZToQQ": "TTZToQQ_TuneCP5_13TeV-amcatnlo-pythia8.root",
+    "WJetsToLNu_HT_100To200": "WJetsToLNu_HT-100To200_TuneCP5_13TeV-madgraphMLM-pythia8.root",
+    "WJetsToLNu_HT_1200To2500": "WJetsToLNu_HT-1200To2500_TuneCP5_13TeV-madgraphMLM-pythia8.root",
+    "WJetsToLNu_HT_200To400": "WJetsToLNu_HT-200To400_TuneCP5_13TeV-madgraphMLM-pythia8.root",
+    "WJetsToLNu_HT_2500ToInf": "WJetsToLNu_HT-2500ToInf_TuneCP5_13TeV-madgraphMLM-pythia8.root",
+    "WJetsToLNu_HT_400To600": "WJetsToLNu_HT-400To600_TuneCP5_13TeV-madgraphMLM-pythia8.root",
+    "WJetsToLNu_HT_600To800": "WJetsToLNu_HT-600To800_TuneCP5_13TeV-madgraphMLM-pythia8.root",
+    "WJetsToLNu_HT_800To1200": "WJetsToLNu_HT-800To1200_TuneCP5_13TeV-madgraphMLM-pythia8.root",
+    "WW": "WW_TuneCP5_13TeV-pythia8.root",
+    "WWW": "WWW_4F_TuneCP5_13TeV-amcatnlo-pythia8.root",
+    "WWZ": "WWZ_4F_TuneCP5_13TeV-amcatnlo-pythia8.root",
+    "WZ": "WZ_TuneCP5_13TeV-pythia8.root",
+    "WZZ": "WZZ_TuneCP5_13TeV-amcatnlo-pythia8.root",
+    "ZZ": "ZZ_TuneCP5_13TeV-pythia8.root",
+    "ZZZ": "ZZZ_TuneCP5_13TeV-amcatnlo-pythia8.root",
+}
+
+self.NEvents = {
+    "WJetsToLNu_HT_800To1200":4684970,
+    "TTTo2L2Nu":86102500,
+    "WW":31787000,
+    "WJetsToLNu_HT_2500ToInf":677141,
+    "QCD_HT1500to2000":19525249,
+    "WZ":13980000,
+    "ST_t_channel_top":116157246,
+    "ZZZ":9031534,
+    "ST_tW_antitop":4823750,
+    "TTZToQQ":5469998,
+    "TTToSemiLeptonic":291469510,
+    "TTZToLLNuNu":5907284,
+    "TTWJetsToLNu":3381080,
+    "WZZ":9321362,
+    "QCD_HT1000to1500":26621202,
+    "WJetsToLNu_HT_100To200":41372969,
+    "WJetsToLNu_HT_600To800":4607946,
+    "ZZ":2384000,
+    "WJetsToLNu_HT_400To600":4745201,
+    "WWW":8945224,
+    "ST_t_channel_antitop":58810964,
+    "WJetsToLNu_HT_1200To2500":4210536,
+    "WWZ":8840222,
+    "QCD_HT2000toInf":10061586,
+    "ST_tW_top":4733742,
+    "WJetsToLNu_HT_200To400":34599960,
+    "TTWJetsToQQ":337793,
+    "ST_s":7774750,
+}
+
+self.XS = {
+    "QCD_HT1000to1500" : 1206*1000,
+    "QCD_HT1500to2000" : 120.4*1000,
+    "QCD_HT2000toInf" : 25.25*1000,
+    "ST_s" : 3.68*1000,
+    "ST_t_channel_antitop" : 80.95*1000,
+    "ST_t_channel_top" : 136.02*1000,
+    "ST_tW_antitop" : 35.6*1000,
+    "ST_tW_top" : 35.6*1000,
+    "TTTo2L2Nu" : 87.31448*1000,
+    "TTToSemiLeptonic" : 364.3508*1000,
+    "TTWJetsToLNu" : None,
+    "TTWJetsToQQ" : None,
+    "TTZToLLNuNu" : None,
+    "TTZToQQ" : None,
+    "WJetsToLNu_HT_100To200" : 1345*1.21*1000,
+    "WJetsToLNu_HT_1200To2500" : 1.329*1.21*1000,
+    "WJetsToLNu_HT_200To400" : 359.7*1.21*1000,
+    "WJetsToLNu_HT_2500ToInf" : 0.03216*1.21*1000,
+    "WJetsToLNu_HT_400To600" : 48.91*1.21*1000,
+    "WJetsToLNu_HT_600To800" : 12.05*1.21*1000,
+    "WJetsToLNu_HT_800To1200" : 5.501*1.21*1000,
+    "WW" : 75.95*1000,
+    "WWW" : 0.2086*1000,
+    "WWZ" : 0.1651*1000,
+    "WZ" : 27.6*1000,
+    "WZZ" : 0.05565*1000,
+    "ZZ" : 12.14*1000,
+    "ZZZ" : 0.01398*1000,
+}
+
+self.color_palet = {
+    'data':1, 
+    'QCD':2,  
+    'Rest':62,  
+    'VV':62, 
+    'STop':8, 
+    'TTbar':80, 
+    'TTV':6, 
+    'WJets':90, 
+    'Signal':1, 
+    'Uncertainty':1,
+    "WWZ":3,
+    "WZZ":7,
+    "WWW":4,
+    "VVV":6,
+}
+
+self.Label = {
+    "TTbar":"t#bar{t}",
+    "WJets":"W+jets ",
+    'data':"data", 
+    'QCD':"QCD",  
+    'Rest':"Rest",  
+    'VV':"VV", 
+    'STop':"STop", 
+    'TTV':"TTV", 
+    "WWZ":"WWZ",
+    "WZZ":"WZZ",
+    "WWW":"WWW",
+    "VVV":"VVV",
+}
+
+self.Signal_Scale = {
+    "WWW":-1,
+    "WWZ":-1,
+    "WZZ":-1,
+}
+
+self.BKG_List = [
+    "WJetsToLNu_HT_100To200" ,
+    "WJetsToLNu_HT_1200To2500" ,
+    "WJetsToLNu_HT_200To400" ,
+    "WJetsToLNu_HT_2500ToInf" ,
+    "WJetsToLNu_HT_400To600" ,
+    "WJetsToLNu_HT_600To800" ,
+    "WJetsToLNu_HT_800To1200" ,
+    "TTTo2L2Nu" ,
+    "TTToSemiLeptonic" ,
+    "ST_s",
+    "ST_t_channel_antitop",
+    "ST_t_channel_top",
+    "ST_tW_antitop",
+    "ST_tW_top",
+    "QCD_HT1000to1500",
+    "QCD_HT1500to2000",
+    "QCD_HT2000toInf",
+    # "TTWJetsToLNu",
+    # "TTWJetsToQQ",
+    # "TTZToLLNuNu",
+    # "TTZToQQ",
+    "WW",
+    "WZ",
+    "ZZ",
+]
+
+self.signal_List = [
+    "WWW",
+    "WWZ",
+    "WZZ",
+]
+
+self.Merge_BKG_Dic = {
+    "WJets" : [
+        "WJetsToLNu_HT_100To200" ,
+        "WJetsToLNu_HT_1200To2500" ,
+        "WJetsToLNu_HT_200To400" ,
+        "WJetsToLNu_HT_2500ToInf" ,
+        "WJetsToLNu_HT_400To600" ,
+        "WJetsToLNu_HT_600To800" ,
+        "WJetsToLNu_HT_800To1200" ,
+    ],
+    "TTbar" : [
+        "TTTo2L2Nu" ,
+        "TTToSemiLeptonic" ,
+    ],
+    "STop" : [
+        "ST_s",
+        "ST_t_channel_antitop",
+        "ST_t_channel_top",
+        "ST_tW_antitop",
+        "ST_tW_top",
+    ],
+    "QCD" : [
+        "QCD_HT1000to1500",
+        "QCD_HT1500to2000",
+        "QCD_HT2000toInf",
+    ],
+    # "TTV" : [
+    #     "TTWJetsToLNu",
+    #     "TTWJetsToQQ",
+    #     "TTZToLLNuNu",
+    #     "TTZToQQ",
+    # ],
+    "VV" : [
+        "WW",
+        "WZ",
+        "ZZ",
+    ]
+}
+
+self.Merge_BKG_Dic_order = [
+    "QCD", "TTV", "VV", "STop", "TTbar", "WJets"
+]
+
+Test = True
+if Test :
+
+    self.signal_List = [
+        "WWW",
+        # "WWZ",
+        # "WZZ",
+    ]
+
+    self.Merge_BKG_Dic = {
+        # "WJets" : [
+        #     "WJetsToLNu_HT_600To800" ,
+        # ],
+        "TTbar" : [
+            "TTToSemiLeptonic" ,
+            # "TTTo2L2Nu" ,
+        ],
+        "WW" : [
+            "WW",
+        ],
+        "WZ" : [
+            "WZ",
+        ]
+    }
+
+    self.BKG_List = [i for j in self.Merge_BKG_Dic for i in self.Merge_BKG_Dic[j]]
+
+    self.Merge_BKG_Dic_order = [
+        "QCD", "TTV", "WZ","WW","VV", "STop", "TTbar", "WJets"
+    ]
+
+    self.Label = {
+        "TTbar":"t#bar{t}",
+        "WJets":"W+jets ",
+        'data':"data", 
+        'QCD':"QCD",  
+        'Rest':"Rest",  
+        'VV':"WW", 
+        'WW':"WW", 
+        'WZ':"WZ", 
+        'STop':"STop", 
+        'TTV':"TTV", 
+        "WWZ":"WWZ",
+        "WZZ":"WZZ",
+        "WWW":"WWW",
+        "VVV":"VVV",
+    }
+
+    self.color_palet = {
+        'data':1, 
+        'QCD':2,  
+        'Rest':62,  
+        'VV':20, 
+        'STop':8, 
+        'TTbar':80, 
+        'TTV':6, 
+        'WJets':90, 
+        'Signal':1, 
+        'Uncertainty':1,
+        "WWZ":3,
+        "WZZ":7,
+        "WWW":4,
+        "VVV":6,
+        "WW":20,
+        "WZ":90,
+    }
