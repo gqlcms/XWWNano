2b03fe55e6b3b33b680f10e054c30c780a4bc9b1
YuzheZhao <60813920+StephenChao@users.noreply.github.com>
2022-01-19 20:10:41 +0800

    Update Transfer_Tree.sh

--- TransferTree/Tool/Transfer_Tree/Transfer_Tree.sh
+++ TransferTree/Tool/Transfer_Tree/Transfer_Tree.sh
@@ -1,2 +1,2 @@
-#python Tool/Transfer_Tree/Transfer_Tree.py -I "/YOUR_INFILE_PATH/InFile.root" -O "/YOUR_OUTFILE_PATH/OutFile.root" -m
-python Tool/Transfer_Tree/Transfer_Tree.py -I "./HWW2q_3m_Skim.root" -O "./HWW2q_3m_Tree.root" -m
+#python ./Tool/Transfer_Tree/Transfer_Tree.py -I "/YOUR_INFILE_PATH/InFile.root" -O "/YOUR_OUTFILE_PATH/OutFile.root" -m
+python ./Tool/Transfer_Tree/Transfer_Tree.py -I "./HWW2q_3m_Skim.root" -O "./HWW2q_3m_Tree.root" -m
