e46a0cb8ceed927fd5a727c3fd7e484cc73fa00a
YuzheZhao <yuzhe.zhao@cern.ch>
2022-07-28 00:43:20 +0800

    Update run_condor.py

--- test/run_condor.py
+++ test/run_condor.py
@@ -95,8 +95,8 @@ def main():
   if not (opt.ismc):
     year_list = ['UL2016_preVFPB','UL2016_preVFPC','UL2016_preVFPD','UL2016_preVFPE','UL2016_preVFPF','UL2016F','UL2016G','UL2016H','UL2017B','UL2017C','UL2017D','UL2017E','UL2017F','UL2018A','UL2018B','UL2018C','UL2018D']
     if opt.year in year_list : 
-      jmeCorrections  = createJMECorrector(opt.ismc,  dataYear=opt.year[:6], runPeriod=opt.year[6:],  jesUncert="Total", jetType="AK8PFPuppi",)
-      jetmetCorrector = createJMECorrector(opt.ismc,   dataYear=opt.year[:6], runPeriod=opt.year[6:], metBranchName="MET")
+      jmeCorrections  = createJMECorrector(opt.ismc,  dataYear=opt.year[:-1], runPeriod=opt.year[-1:],  jesUncert="Total", jetType="AK8PFPuppi",)
+      jetmetCorrector = createJMECorrector(opt.ismc,   dataYear=opt.year[:-1], runPeriod=opt.year[-1:], metBranchName="MET")
     if opt.year in ["UL2016_preVFPB","UL2016_preVFPC","UL2016_preVFPD","UL2016_preVFPE","UL2016_preVFPF"]:
       p = PostProcessor(opt.output, [opt.inputs], modules=[muonScaleRes2016a(),jetmetCorrector(),jmeCorrections(),VVV2016()], provenance=True,fwkJobReport=True, jsonInput=jsoninput)
 
